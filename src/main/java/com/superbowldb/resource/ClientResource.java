b7cb54f95f3115b47a202d80f4208e8da895a744
dispatch404 <virajsamson4@gmail.com>
2022-06-21 17:47:51 -0500

    modified routes

--- src/main/java/com/superbowldb/resource/ClientResource.java
+++ src/main/java/com/superbowldb/resource/ClientResource.java
@@ -2,17 +2,16 @@ package com.superbowldb.resource;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.superbowldb.service.ClientService;
 
 @RestController
-@RequestMapping("/api")
 public class ClientResource {
 	
 	private final ClientService clientService;
